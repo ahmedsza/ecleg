d8d3687a36f3c1727094eac3cfae5a38afeea23a
Ahmed Salijee <ahmeds@microsoft.com>
2025-12-17 12:02:16 +0200

    Fix static web app location variable in resource creation script

--- createresources.ps1
+++ createresources.ps1
@@ -8,6 +8,7 @@ $frontEndRepositoryUrl = "https://dev.azure.com/eclegislature/Website/_git/ecpl-
 $cmsRepositoryUrl = "https://dev.azure.com/eclegislature/Website/_git/ecpl-content-service.git"
 $environment = "qa"
 $resourceGroupName = "ecpl-websites-rg-$environment"
+$staticWebAppLocation="westeurope"
 
 # create a resource group using az cli
 az group create --name $resourceGroupName --location $location
@@ -17,11 +18,11 @@ Write-Output "Resource group '$resourceGroupName' created in location '$location
 
 # create a standard static web app using az cli
 $frontEndStaticWebAppName = "ecpl-frontend-$environment"
-az staticwebapp create --name $frontEndStaticWebAppName --resource-group $resourceGroupName --location $location --source $frontEndRepositoryUrl --sku $staticwebSiteSku --source $frontEndRepositoryUrl --login-with-ado --branch main
+az staticwebapp create --name $frontEndStaticWebAppName --resource-group $resourceGroupName --location $staticWebAppLocation --source $frontEndRepositoryUrl --sku $staticwebSiteSku --source $frontEndRepositoryUrl --login-with-ado --branch main
 Write-Output "Static web app '$frontEndStaticWebAppName' created in resource group '$resourceGroupName'."
 
 # create a second static web app using az cli
 $cmsStaticWebAppName = "ecpl-cms-$environment"
-az staticwebapp create --name $cmsStaticWebAppName --resource-group $resourceGroupName  --location $location --source $cmsRepositoryUrl --sku $staticwebSiteSku --source $cmsRepositoryUrl --login-with-ado --branch main
+az staticwebapp create --name $cmsStaticWebAppName --resource-group $resourceGroupName  --location $staticWebAppLocation --source $cmsRepositoryUrl --sku $staticwebSiteSku --source $cmsRepositoryUrl --login-with-ado --branch main
 Write-Output "Static web app '$cmsStaticWebAppName' created in resource group '$resourceGroupName'."
 
