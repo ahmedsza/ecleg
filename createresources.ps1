2d61c35547425e4a5d77abcc9bac16f76af2fda5
Ahmed Salijee <ahmeds@microsoft.com>
2025-12-17 11:56:51 +0200

    Fix command syntax for creating static web apps in Azure CLI

--- createresources.ps1
+++ createresources.ps1
@@ -1,4 +1,6 @@
 # create variables for resource group name and location
+# be sure to do az login before running this script
+# run az account show to make sure you run az account show to verify
 
 $location = "southafricanorth"
 $staticwebSiteSku = "Standard"  
@@ -15,11 +17,11 @@ Write-Output "Resource group '$resourceGroupName' created in location '$location
 
 # create a standard static web app using az cli
 $frontEndStaticWebAppName = "ecpl-frontend-$environment"
-az staticwebapp create --name $frontEndStaticWebAppName --resource-group $resourceGroupName --location $location --source --sku $staticwebSiteSku --source $frontEndRepositoryUrl --login-with-ado
+az staticwebapp create --name $frontEndStaticWebAppName --resource-group $resourceGroupName --location $location --source $frontEndRepositoryUrl --sku $staticwebSiteSku --source $frontEndRepositoryUrl --login-with-ado
 Write-Output "Static web app '$frontEndStaticWebAppName' created in resource group '$resourceGroupName'."
 
 # create a second static web app using az cli
 $cmsStaticWebAppName = "ecpl-cms-$environment"
-az staticwebapp create --name $cmsStaticWebAppName --resource-group $resourceGroupName  --location $location --source --sku $staticwebSiteSku --source $cmsRepositoryUrl --login-with-ado
+az staticwebapp create --name $cmsStaticWebAppName --resource-group $resourceGroupName  --location $location --source $cmsRepositoryUrl --sku $staticwebSiteSku --source $cmsRepositoryUrl --login-with-ado
 Write-Output "Static web app '$cmsStaticWebAppName' created in resource group '$resourceGroupName'."
 
