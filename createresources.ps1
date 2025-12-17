942c590018f148fb1a5c66e9a3daa3a8c571b30d
Ahmed Salijee <ahmeds@microsoft.com>
2025-12-17 12:20:55 +0200

    Add variables for storage account and app service; update branch names for static web apps

--- createresources.ps1
+++ createresources.ps1
@@ -9,6 +9,16 @@ $cmsRepositoryUrl = "https://dev.azure.com/eclegislature/Website/_git/ecpl-conte
 $environment = "qa"
 $resourceGroupName = "ecpl-websites-rg-$environment"
 $staticWebAppLocation="westeurope"
+$frontEndBranchName="main"
+$cmsBranchName="main"
+$storageAccountName = "ecplstorage$environment"
+$appServicePlanName = "ecpl-appserviceplan-$environment"
+$appServiceName = "ecpl-appservice-$environment"
+$keyVaultName = "ecpl-keyvault-$environment"
+$mysqlServerName = "ecplmysqlserver$environment"
+$mysqlDatabaseName = "ecplmysqldb$environment"
+$storageSku = "ZRS"  # Zone Redundant Storage
+
 
 # create a resource group using az cli
 az group create --name $resourceGroupName --location $location
@@ -18,11 +28,21 @@ Write-Output "Resource group '$resourceGroupName' created in location '$location
 
 # create a standard static web app using az cli
 $frontEndStaticWebAppName = "ecpl-frontend-$environment"
-az staticwebapp create --name $frontEndStaticWebAppName --resource-group $resourceGroupName --location $staticWebAppLocation --source $frontEndRepositoryUrl --sku $staticwebSiteSku --source $frontEndRepositoryUrl --login-with-ado --branch main
+az staticwebapp create --name $frontEndStaticWebAppName --resource-group $resourceGroupName --location $staticWebAppLocation --source $frontEndRepositoryUrl --sku $staticwebSiteSku --source $frontEndRepositoryUrl --login-with-ado --branch $frontEndBranchName
 Write-Output "Static web app '$frontEndStaticWebAppName' created in resource group '$resourceGroupName'."
 
 # create a second static web app using az cli
 $cmsStaticWebAppName = "ecpl-cms-$environment"
-az staticwebapp create --name $cmsStaticWebAppName --resource-group $resourceGroupName  --location $staticWebAppLocation --source $cmsRepositoryUrl --sku $staticwebSiteSku --source $cmsRepositoryUrl --login-with-ado --branch main
+az staticwebapp create --name $cmsStaticWebAppName --resource-group $resourceGroupName  --location $staticWebAppLocation --source $cmsRepositoryUrl --sku $staticwebSiteSku --source $cmsRepositoryUrl --login-with-ado --branch $cmsBranchName
 Write-Output "Static web app '$cmsStaticWebAppName' created in resource group '$resourceGroupName'."
+
+# create a storage account for all the documents using zone redundant storage
+az storage account create --name $storageAccountName --resource-group $resourceGroupName --location $location --sku $storageSku --kind StorageV2
+Write-Output "Storage account '$storageAccountName' created in resource group '$resourceGroupName'."
+
+# create an app service
+
+# create a keyvault
+
+# create a mysql flexible server and database
 
